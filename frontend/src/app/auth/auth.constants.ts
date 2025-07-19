2558171acb4202f7d919b9ffda3b70630fe5cad8
Mateusz Brankiewicz <89447777+MateuszBrankiewicz@users.noreply.github.com>
2025-07-19 12:21:36 +0200

    fixes and simple register logic impl with error message display (#4)

---
+++ frontend/src/app/auth/auth.constants.ts
@@ -0,0 +1,15 @@
+interface RegisterUser{
+    name: string,
+    surname: string,
+    govermentId: string,
+    birthDate: string | null,
+    province: string,
+    postalCode : string,
+    city: string,
+    number: string,
+    street: string,
+    phoneNumber: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+}
