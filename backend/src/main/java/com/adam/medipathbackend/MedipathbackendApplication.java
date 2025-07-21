e8ac42af178b6d6c68f7d537be40e602ba24eea8
ThePowerOf76 <s99507@pollub.edu.pl>
2025-07-21 10:55:58 +0200

    Add cities controller and tests

--- backend/src/main/java/com/adam/medipathbackend/MedipathbackendApplication.java
+++ backend/src/main/java/com/adam/medipathbackend/MedipathbackendApplication.java
@@ -1,19 +1,32 @@
 package com.adam.medipathbackend;
 
+import com.adam.medipathbackend.models.City;
+import com.adam.medipathbackend.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api")
+@EnableMongoRepositories
 public class MedipathbackendApplication {
 
-	 @RequestMapping("/")
-  public String home() {
-    return "Test";
-  }
+	@GetMapping("/")
+  	public String home() {
+		return "API OK";
+  	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(MedipathbackendApplication.class, args);
 	}
 
--- backend/src/test/java/com/adam/medipathbackend/MedipathbackendApplicationTests.java
+++ backend/src/test/java/com/adam/medipathbackend/MedipathbackendApplicationTests.java
@@ -1,13 +1,83 @@
 package com.adam.medipathbackend;
 
+import com.adam.medipathbackend.controllers.CityController;
+import com.adam.medipathbackend.models.City;
+import com.adam.medipathbackend.repository.CityRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = CityController.class)
 class MedipathbackendApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private MockMvc mvc;
+
+	@MockitoBean
+	private CityRepository cityRepository;
+
+
+	private List<City> cityList;
+
+	@BeforeEach
+    public void createTestData() {
+		this.cityList = new ArrayList<>();
+		this.cityList.add(new City( "Lublin"));
+		this.cityList.add(new City( "Lubin"));
+		this.cityList.add(new City( "Warszawa"));
 	}
+	@Test
+	public void getAllCities() throws Exception {
+
+		when(cityRepository.findAll()).thenReturn(cityList);
+
+		mvc.perform(get("/api/cities").contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.size()").value(3))
+				.andExpect(jsonPath("$[0].name").value("Lublin"))
+				.andExpect(jsonPath("$[1].name").value("Lubin"))
+				.andExpect(jsonPath("$[2].name").value("Warszawa"));
+
+	}
+	@Test
+	public void getRegexCities() throws Exception {
+
+		when(cityRepository.findAll("Lub")).thenReturn(cityList.subList(0, 1));
+
+		mvc.perform(get("/api/cities").contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.size()").value(2))
+				.andExpect(jsonPath("$[0].name").value("Lublin"))
+				.andExpect(jsonPath("$[1].name").value("Lubin"));
+
+	}
+	@Test
+	public void getInvalidCity() throws Exception {
+
+		when(cityRepository.findAll("Abcd")).thenReturn(new ArrayList<>());
+
+		mvc.perform(get("/api/cities").contentType("application/json"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.size()").value(0));
+	}
+
+
 
 }
