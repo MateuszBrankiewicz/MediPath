0818d1eca18eda7a9000ba5c6840f452b0796110
ThePowerOf76 <s99507@pollub.edu.pl>
2025-08-01 19:25:45 +0200

    Add password reset tests

--- backend/src/test/java/com/adam/medipathbackend/MedipathbackendApplicationTests.java
+++ backend/src/test/java/com/adam/medipathbackend/MedipathbackendApplicationTests.java
@@ -1,20 +1,37 @@
 package com.adam.medipathbackend;
 
+import com.adam.medipathbackend.config.CORSConfig;
+import com.adam.medipathbackend.config.HttpSessionConfig;
+import com.adam.medipathbackend.config.MailConfig;
 import com.adam.medipathbackend.controllers.CityController;
 import com.adam.medipathbackend.models.City;
+import com.adam.medipathbackend.models.PasswordResetEntry;
 import com.adam.medipathbackend.models.User;
 import com.adam.medipathbackend.repository.CityRepository;
+import com.adam.medipathbackend.repository.PasswordResetEntryRepository;
 import com.adam.medipathbackend.repository.UserRepository;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
+import jakarta.mail.internet.MimeMessage;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.test.context.*;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,7 +39,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,6 +52,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 class MedipathbackendApplicationTests {
 
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("user", "admin"))
+            .withPerMethodLifecycle(false);
+
+    @DynamicPropertySource
+    static void configureMailHost(DynamicPropertyRegistry registry) {
+        registry.add("spring.mail.host", () -> greenMail.getSmtp().getBindTo());
+        registry.add("spring.mail.port", () -> greenMail.getSmtp().getPort());
+    }
+
     @Autowired
     private MockMvc mvc;
 
@@ -41,6 +71,9 @@ class MedipathbackendApplicationTests {
 
     @MockitoBean
     private UserRepository userRepository;
+
+    @MockitoBean
+    private PasswordResetEntryRepository preRepository;
 
     private List<City> cityList;
 
@@ -268,5 +301,79 @@ class MedipathbackendApplicationTests {
     }
 
 
+    @Test
+    public void tryResetMailNoParam() throws Exception {
+
+
+        mvc.perform(get("/api/users/resetpassword"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("missing email in request parameters"));
+
+    }
+
+    @Test
+    public void tryResetMailValid() throws Exception {
+
+       when(userRepository.findByEmail("test@mail.com")).thenReturn(Optional.of(new User("test@mail.com", "A", "B", "1234567890", LocalDate.of(1900, 12, 12), "A", "A", "A", "A", "A", "A", "$argon2id$v=19$m=60000,t=10,p=1$MOrLn9JdvWpJyJDMZ4Z9qg$zgHTASZaQH9zoTUO0bd0re+6G523ZUreKFmWQSu+f24")));
+
+
+        mvc.perform(get("/api/users/resetpassword?address=test@mail.com"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("password reset mail has been sent, if the account exists"));
+
+        Awaitility.await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            MimeMessage receivedMessage = greenMail.getReceivedMessages()[0];
+
+            assertEquals(1, receivedMessage.getAllRecipients().length);
+            assertEquals("test@mail.com", receivedMessage.getAllRecipients()[0].toString());
+            assertEquals("MediPath <service@medipath.com>", receivedMessage.getFrom()[0].toString());
+            assertEquals("Password reset request", receivedMessage.getSubject());
+        });
+
+    }
+
+
+    @Test
+    public void tryResetMailSecondaryNoData() throws Exception {
+
+        String exampleLogin = "{}";
+
+        mvc.perform(post("/api/users/resetpassword").contentType(MediaType.APPLICATION_JSON).content(exampleLogin))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("missing fields in request body"))
+                .andExpect(jsonPath("$.fields[0]").value("token"))
+                .andExpect(jsonPath("$.fields[1]").value("password"));
+    }
+
+    @Test
+    public void tryResetMailSecondaryValidToken() throws Exception {
+
+        when(preRepository.findValidToken("1234567890abcdef")).thenReturn(Optional.of(new PasswordResetEntry("test@mail.com", "1234567890abcdef")));
+        when(userRepository.findByEmail("test@mail.com")).thenReturn(Optional.of(new User("test@mail.com", "A", "B", "1234567890", LocalDate.of(1900, 12, 12), "A", "A", "A", "A", "A", "A", "$argon2id$v=19$m=60000,t=10,p=1$MOrLn9JdvWpJyJDMZ4Z9qg$zgHTASZaQH9zoTUO0bd0re+6G523ZUreKFmWQSu+f24")));
+
+
+        String exampleLogin = "{" +
+                "\"token\": \"1234567890abcdef\"," +
+                "\"password\": \"anotherpassword\"" +
+                "}";
+
+        mvc.perform(post("/api/users/resetpassword").contentType(MediaType.APPLICATION_JSON).content(exampleLogin))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("password reset successfully"));
+    }
+    @Test
+    public void tryResetMailSecondaryInvalidToken() throws Exception {
+
+        when(preRepository.findValidToken("1234567890abcdef")).thenReturn(Optional.empty());
+
+        String exampleLogin = "{" +
+                "\"token\": \"1234567890abcdef\"," +
+                "\"password\": \"anotherpassword\"" +
+                "}";
+
+        mvc.perform(post("/api/users/resetpassword").contentType(MediaType.APPLICATION_JSON).content(exampleLogin))
+                .andExpect(status().isGone())
+                .andExpect(jsonPath("$.message").value("token invalid or expired"));
+    }
 
 }
