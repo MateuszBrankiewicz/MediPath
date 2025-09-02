7a27f0cafb38d70d266497b6ca290d20fc179430
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-02 13:36:15 +0200

    Removed project statements from InstitutionRepository queries

--- backend/src/main/java/com/adam/medipathbackend/repository/InstitutionRepository.java
+++ backend/src/main/java/com/adam/medipathbackend/repository/InstitutionRepository.java
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 
 public interface InstitutionRepository extends MongoRepository<Institution, String> {
 
-    @Aggregation({"{$project: { name: 1 }}","{ $match: { name: { $regex: /?0/i }}}"})
+    @Aggregation({"{$match: { name: { $regex: /?0/i }}}"})
     ArrayList<Institution> findInstitutionByName(String name);
 
-    @Aggregation({"{$project: { name: 1, types: 1}}", "{$match: { types: { $elemMatch: { $regex: /?0/i }}}}"})
+    @Aggregation({"{$match: { types: { $elemMatch: { $regex: /?0/i }}}}"})
     ArrayList<Institution> findInstitutionBySpec(String name);
+
+
 }
