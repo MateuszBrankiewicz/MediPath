04bb9f91c4d0e03e4c3e046c4f7d7119e4d50ecf
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-02 13:39:31 +0200

    Change object property modification functions for Users, Institutions and Addresses and add comparison

--- backend/src/main/java/com/adam/medipathbackend/models/Institution.java
+++ backend/src/main/java/com/adam/medipathbackend/models/Institution.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 @Document("Institution")
 public class Institution {
@@ -24,19 +25,29 @@ public class Institution {
 
     private String image;
 
+    private int numOfRatings;
 
-    public Institution(String name, boolean isPublic, Address address, float rating, String image) {
+    public Institution(String name, boolean isPublic, Address address, String image) {
         this.name = name;
         this.isPublic = isPublic;
         this.address = address;
-        this.rating = rating;
+        this.rating = 0;
         this.image = image;
         this.types = new ArrayList<>();
         this.employees = new ArrayList<>();
+        this.numOfRatings = 0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public void setName(String name) {
@@ -71,17 +82,20 @@ public class Institution {
         return employees;
     }
 
-    public void setEmployees(ArrayList<StaffDigest> employees) {
-        this.employees = employees;
+    public void addEmployee(StaffDigest staff) {
+        employees.add(staff);
     }
 
     public float getRating() {
         return rating;
     }
 
-    public void setRating(float rating) {
-        this.rating = rating;
+    public void addRating(float newRating) {
+        this.rating = ((this.rating * this.numOfRatings) + newRating) / (this.numOfRatings + 1);
+        this.numOfRatings += 1;
     }
+
+
 
     public String getImage() {
         return image;
@@ -89,5 +103,17 @@ public class Institution {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Institution that = (Institution) o;
+        return isPublic == that.isPublic && Objects.equals(name, that.name) && Objects.equals(address, that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, isPublic, address);
     }
 }
