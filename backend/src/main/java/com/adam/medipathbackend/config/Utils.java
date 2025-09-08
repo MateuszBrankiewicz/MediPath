fb83a6553fe3333440af9b176737a501309895c6
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-08 23:14:19 +0200

    Changed variable names in Levenshtein distance function

--- backend/src/main/java/com/adam/medipathbackend/config/Utils.java
+++ backend/src/main/java/com/adam/medipathbackend/config/Utils.java
@@ -5,29 +5,29 @@ public class Utils {
     static final int MINIMUM_ACCEPTABLE_SIMILARITY_THRESHOLD = 5;
 
     static int compute_Levenshtein_distance(String str1, String str2) {
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
-        int subCost = 0;
+        int[][] distanceMatrix = new int[str1.length() + 1][str2.length() + 1];
+        int substitutionCost = 0;
 
         for (int i = 0; i <= str1.length(); i++) {
             for (int j = 0; j <= str2.length(); j++) {
 
                 if (i == 0) {
-                    dp[i][j] = j;
+                    distanceMatrix[i][j] = j;
 
                 } else if (j == 0) {
-                    dp[i][j] = i;
+                    distanceMatrix[i][j] = i;
 
                 } else {
-                    subCost = str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1;
+                    substitutionCost = str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1;
 
-                    dp[i][j] = min(dp[i - 1][j - 1] + subCost,
-                            dp[i - 1][j] + 1,
-                            dp[i][j - 1] + 1);
+                    distanceMatrix[i][j] = min(distanceMatrix[i - 1][j - 1] + substitutionCost,
+                            distanceMatrix[i - 1][j] + 1,
+                            distanceMatrix[i][j - 1] + 1);
                 }
             }
         }
 
-        return dp[str1.length()][str2.length()];
+        return distanceMatrix[str1.length()][str2.length()];
     }
     static int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
