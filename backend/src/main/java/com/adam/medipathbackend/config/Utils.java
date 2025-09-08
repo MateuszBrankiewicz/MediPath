ae48b10e3aa0b5e3f20c4fa129607fa8b0079fd3
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-08 22:59:52 +0200

    Improved formatting in Levenshtein distance function

--- backend/src/main/java/com/adam/medipathbackend/config/Utils.java
+++ backend/src/main/java/com/adam/medipathbackend/config/Utils.java
@@ -7,14 +7,19 @@ public class Utils {
     static int compute_Levenshtein_distance(String str1, String str2) {
         int[][] dp = new int[str1.length() + 1][str2.length() + 1];
         int subCost = 0;
+
         for (int i = 0; i <= str1.length(); i++) {
             for (int j = 0; j <= str2.length(); j++) {
+
                 if (i == 0) {
                     dp[i][j] = j;
+
                 } else if (j == 0) {
                     dp[i][j] = i;
+
                 } else {
                     subCost = str1.charAt(i - 1) == str2.charAt(j - 1) ? 0 : 1;
+
                     dp[i][j] = min(dp[i - 1][j - 1] + subCost,
                             dp[i - 1][j] + 1,
                             dp[i][j - 1] + 1);
