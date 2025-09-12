621705c130705274d33850b043fcd6ff338689bb
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-12 19:28:04 +0200

    Added endpoint for getting active codes

--- backend/src/main/java/com/adam/medipathbackend/repository/VisitRepository.java
+++ backend/src/main/java/com/adam/medipathbackend/repository/VisitRepository.java
@@ -2,14 +2,19 @@ package com.adam.medipathbackend.repository;
 
 import com.adam.medipathbackend.models.User;
 import com.adam.medipathbackend.models.Visit;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Optional;
 
 public interface VisitRepository extends MongoRepository<Visit, String> {
 
     @Query("{'patient.patientId': ?0, 'time.startTime': { $gt: new Date() }}")
     ArrayList<Visit> getUpcomingVisits(String patientID);
+
+    @Aggregation({"{ $unwind: { path: \"$codes\" } }", " { $match: { \"patient.userId\": \"?0\", \"codes.isActive\": true } }", "{ $project:  { \"codes.codeType\": 1, \"codes.code\": 1, _id: 0 } }"})
+    ArrayList<Map<String, Object>> getActiveCodesForPatient(String patientID);
 }
