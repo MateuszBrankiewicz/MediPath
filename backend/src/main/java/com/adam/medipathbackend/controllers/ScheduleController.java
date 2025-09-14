973051a13620c6b8e1b6f3afa6c3f68488382f15
ThePowerOf76 <s99507@pollub.edu.pl>
2025-09-14 11:40:33 +0200

    Simplified schedule adding endpoint

--- backend/src/main/java/com/adam/medipathbackend/controllers/ScheduleController.java
+++ backend/src/main/java/com/adam/medipathbackend/controllers/ScheduleController.java
@@ -1,9 +1,8 @@
 package com.adam.medipathbackend.controllers;
 
 import com.adam.medipathbackend.config.Utils;
-import com.adam.medipathbackend.models.Schedule;
-import com.adam.medipathbackend.models.User;
-import com.adam.medipathbackend.models.Visit;
+import com.adam.medipathbackend.models.*;
+import com.adam.medipathbackend.repository.InstitutionRepository;
 import com.adam.medipathbackend.repository.ScheduleRepository;
 import com.adam.medipathbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,30 +25,46 @@ public class ScheduleController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    InstitutionRepository institutionRepository;
+
     @PostMapping("/add")
-    public ResponseEntity<Map<String, Object>> add(@RequestBody Schedule schedule) {
+    public ResponseEntity<Map<String, Object>> add(@RequestBody AddScheduleForm schedule) {
         ArrayList<String> missingFields = new ArrayList<>();
 
         if(schedule.getEndHour() == null) {
             missingFields.add("endHour");
         }
-        if(schedule.getDoctor() == null || !schedule.getDoctor().isValid()) {
+        if(schedule.getDoctorID() == null || schedule.getDoctorID().isBlank()) {
             missingFields.add("doctor");
         }
         if(schedule.getStartHour() == null) {
             missingFields.add("startHour");
         }
-        if(schedule.getInstitution() == null || !schedule.getInstitution().isValid()) {
+        if(schedule.getInstitutionID() == null || schedule.getInstitutionID().isBlank()) {
             missingFields.add("institution");
         }
         if(!missingFields.isEmpty()) {
             return new ResponseEntity<>(Map.of("message", "missing fields in request body", "fields", missingFields), HttpStatus.BAD_REQUEST);
         }
-        Optional<Schedule> isDoctorBookedThen = scheduleRepository.checkScheduleDuplicate(schedule.getStartHour(), schedule.getDoctor().getUserId());
+
+        Optional<User> optUser = userRepository.findById(schedule.getDoctorID());
+        if(optUser.isEmpty()) {
+            return new ResponseEntity<>(Map.of("message", "invalid doctor id"), HttpStatus.BAD_REQUEST);
+        }
+
+        Optional<Institution> optInst = institutionRepository.findById(schedule.getInstitutionID());
+        if(optInst.isEmpty()) {
+            return new ResponseEntity<>(Map.of("message", "invalid institution id"), HttpStatus.BAD_REQUEST);
+
+        }
+        Optional<Schedule> isDoctorBookedThen = scheduleRepository.checkScheduleDuplicate(schedule.getStartHour(), schedule.getDoctorID());
         if(isDoctorBookedThen.isPresent()) {
             return new ResponseEntity<>(Map.of("message", "this doctor is booked at this hour"), HttpStatus.CONFLICT);
         }
-        scheduleRepository.save(schedule);
+        User doctor = optUser.get();
+        Schedule newSchedule = new Schedule(schedule.getStartHour(), schedule.getEndHour(), new DoctorDigest(schedule.getDoctorID(), doctor.getName(), doctor.getSurname(), doctor.getSpecialisations()), new InstitutionDigest(schedule.getInstitutionID(), optInst.get().getName()));
+        scheduleRepository.save(newSchedule);
         return new ResponseEntity<>(Map.of("message", "success"), HttpStatus.CREATED);
 
     }
