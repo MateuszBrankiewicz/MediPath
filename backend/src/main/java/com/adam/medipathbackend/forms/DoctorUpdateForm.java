55db6719b8e311906307b87a4971dc313585b826
ThePowerOf76 <s99507@pollub.edu.pl>
2025-11-14 13:03:18 +0100

    Doctor's specialisatios are now aggregated from institutions and changed syntax of employee registration

--- backend/src/main/java/com/adam/medipathbackend/forms/DoctorUpdateForm.java
+++ backend/src/main/java/com/adam/medipathbackend/forms/DoctorUpdateForm.java
@@ -1,29 +1,5 @@
 package com.adam.medipathbackend.forms;
 
-import java.util.ArrayList;
+public record DoctorUpdateForm(String licenceNumber) {
 
-public class DoctorUpdateForm {
-    private String licenceNumber;
-    private ArrayList<String> specialisations;
-
-    public DoctorUpdateForm(String licenceNumber, ArrayList<String> specialisations) {
-        this.licenceNumber = licenceNumber;
-        this.specialisations = specialisations;
-    }
-
-    public String getLicenceNumber() {
-        return licenceNumber;
-    }
-
-    public void setLicenceNumber(String licenceNumber) {
-        this.licenceNumber = licenceNumber;
-    }
-
-    public ArrayList<String> getSpecialisations() {
-        return specialisations;
-    }
-
-    public void setSpecialisations(ArrayList<String> specialisations) {
-        this.specialisations = specialisations;
-    }
 }
